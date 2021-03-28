f90c1cc97373597ee6346d0f41d05a0cd9ea6061
fdAssis <fd07ass@gmail.com>
2021-03-28 14:44:07 -0300

    created index

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import FluentSQLBuilder from './fluentSQL.js';
 const result = FluentSQLBuilder.for(data)
   .where({ registered: /^(2020|2019)/ })
   .where({ category: /^(security|developer|quality assurance)$/ })
+  .where({ phone: /\((852|890|810)\)/ })
+  .select(['name', 'company', 'phone', 'category', 'registered'])
+  .orderBy('category')
+  .limit(2)
   .build();
 
 console.table(result);
