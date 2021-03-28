f3fb2cce9af70f9f743a3721091b08057d55678d
fdAssis <fd07ass@gmail.com>
2021-03-28 00:13:08 -0300

    test SQL where

--- test/fluentSQL.test.js
+++ test/fluentSQL.test.js
@@ -1,7 +1,69 @@
 import { expect, describe, test } from '@jest/globals';
+import FluentSQLBuild from '../src/fluentSQL.js';
+
+const data = [
+  {
+    id: 0,
+    name: 'Francisco',
+    category: 'Developer',
+  },
+  {
+    id: 1,
+    name: 'Maria',
+    category: 'Developer',
+  },
+  {
+    id: 2,
+    name: 'Joao',
+    category: 'Manager',
+  },
+];
 
 describe('Test Suit for FluentSQL Builder', () => {
-  test('hello', () => {
-    expect(1).toBe(1);
+  test('#for should return a FluentSQLBuild instance', () => {
+    const result = FluentSQLBuild.for(data);
+    const expected = new FluentSQLBuild({ database: data });
+    expect(result).toStrictEqual(expected);
   });
+
+  test('#build should return the empty object instance', () => {
+    const result = FluentSQLBuild.for(data).build();
+    const expected = data;
+    expect(result).toStrictEqual(expected);
+  });
+
+  test('#limit given a collection is should limit results', () => {
+    const result = FluentSQLBuild.for(data).limit(1).build();
+    const expected = [data[0]];
+
+    expect(result).toStrictEqual(expected);
+  });
+
+  test('#where given a collection it should filter data', () => {
+    const result = FluentSQLBuild.for(data)
+      .where({
+        category: /^Dev/,
+      })
+      .build();
+
+    const expected = data.filter(
+      ({ category }) => category.slice(0, 3) === 'Dev'
+    );
+
+    expect(result).toStrictEqual(expected);
+  });
+
+  test('#select given a collection it should return only specific fields', () => {
+    const result = FluentSQLBuild.for(data)
+      .select(['name', 'category'])
+      .build();
+
+    const expected = data.map(({ name, category }) => ({ name, category }));
+
+    expect(result).toStrictEqual(expected);
+  });
+
+  test.todo('#orderBy given a collection in should order results by field');
+
+  test.todo('pipeline');
 });
